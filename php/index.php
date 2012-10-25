9d600d4272e02f4b969d2d387ed25d2166cbdcd8
Ralph Schindler <ralph@ralphschindler.com>
2012-10-24 21:15:18 -0700

    Openshift updates

--- php/index.php
+++ php/index.php
@@ -1,2 +1,9 @@
 <?php
-phpinfo();
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../source/autoload.php';
+
+$app = new MiniP\Application(
+    new RantHub\ApplicationContext(realpath(__DIR__ . '/../'), 'development')
+);
+$app->run();
